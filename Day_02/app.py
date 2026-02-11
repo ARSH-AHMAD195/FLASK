c18754a9967703a9261381eb5c73d17f7295fb25
Arsh Ahmad <arshahmad10b36862@gmail.com>
2026-02-11 12:33:02 +0530

    Added Day 02

---
+++ Day_02/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def landing_page():
+    return "this is landing page"
+
+@app.route("/welcome/<name>")
+def home_page(name:str):
+    text = "Hello I'm admin this side, welcome to the site."
+    fruits = ["apple", "mango", "banana"]
+    num1 = 10.41
+    num2 = 20.35
+    nums = [10,22,31,47,59]
+    return render_template("welcome.html" ,name = name, text=text, fruits = fruits, num1 = num1, num2 = num2, nums = nums)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
